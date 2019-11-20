b7182424c7af7aba24937021616aaa1a157697fe
Toby <tobyau9000@gmail.com>
2019-11-19 22:53:06 -0500

    add featured blog component for home page

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,31 +8,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
-
-// import Header from "./header"
-import Nav from "../components/nav"
+import Nav from "../components/Nav"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
 
   return (
     <div className="layout-container">
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <Nav />
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 1300,
-          // padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
         }}
       >
